07e722d16138ea4eaec0de4059e61755c9d9ce75
Paul-Otto Müller <pmueller@sim.informatik.tu-darmstadt.de>
2024-05-14 08:33:22 +0200

    Add descriptions

--- include/approximate_boxes/utils.h
+++ include/approximate_boxes/utils.h
@@ -1,6 +1,14 @@
-//
-// Created by paul on 13.05.24.
-//
+/**
+ * @file utils.h
+ * @brief
+ *
+ * Copyright (c) 2024 Paul-Otto Müller <https://github.com/paulotto>
+ * License: LGPL-3.0-or-later (https://github.com/paulotto/approximate_boxes/blob/main/LICENSE)
+ * URL: https://github.com/paulotto/approximate_boxes/blob/main/include/approximate_boxes/utils.h
+ *
+ * @author Paul-Otto Müller
+ * @date 13.05.2024
+ */
 
 #ifndef UTILS_H
 #define UTILS_H
@@ -12,10 +20,15 @@ namespace approx_boxes::utils {
 #define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 #define PBWIDTH 60
 
+    /**
+     * @brief Prints a progress bar to the console.
+     * @param percentage The percentage of the progress.
+     */
     inline void PrintProgress(double percentage) {
-        int val = static_cast<int>(percentage * 100);
-        int lpad = static_cast<int>(percentage * PBWIDTH);
-        int rpad = PBWIDTH - lpad;
+        const int val = static_cast<int>(percentage * 100);
+        const int lpad = static_cast<int>(percentage * PBWIDTH);
+        const int rpad = PBWIDTH - lpad;
+
         printf("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
         fflush(stdout);
     }
